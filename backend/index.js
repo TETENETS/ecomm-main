97265baba3b3f690aadb2b5635dc2597cb57a6ff
TETENETS <tetenet.internet@gmail.com>
2026-06-25 23:26:30 -0400

    Comment out cors console log
    Comment out the console log for allowed origins.

--- backend/index.js
+++ backend/index.js
@@ -2238,7 +2238,7 @@ app.post('/api/logs', async (req, res) => {
   }
 });
 
-console.log(`cors:${allowedOrigins}`)
+//console.log(`cors:${allowedOrigins}`)
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
